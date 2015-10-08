626e32baf7952cde7137efcb9d19a6789f552f2f
GW9999 <GW9999@10.242.69.215>
2015-10-08 11:30:34 -0400

    Money V3

--- src/vendingmachine/Money.java
+++ src/vendingmachine/Money.java
@@ -28,19 +28,34 @@ public class Money {
     public void determine (double input, double price){
         input -= price;
         //run through each denomination and determine how much of each coin can be used
-        usedToonie = (int)(input/2.0);
+        if ((int)(input/2.0) <= numToonies)
+            usedToonie = (int)(input/2.0);
+        else
+            usedToonie = numToonies;
         input -= 2.0*usedToonie;
         numToonies -= usedToonie;
-        usedLoonie = (int)(input/1.0);
+        if ((int)(input/1.0) <= numLoonies)
+            usedLoonie = (int)(input/1.0);
+        else
+            usedLoonie = numLoonies;
         input -= 1.0*usedLoonie;
         numLoonies -= usedLoonie;
-        usedQuarter = (int)(input/0.25);
+        if ((int)(input/0.25) <= numQuarters)
+            usedQuarter = (int)(input/0.25);
+        else
+            usedQuarter = numQuarters;
         input -= 0.25*usedQuarter;
         numQuarters -= usedQuarter;
-        usedDime = (int)(input/0.10);
+        if ((int)(input/0.10) <= numDime)
+            usedDime = (int)(input/0.10);
+        else
+            usedDime = numDime;
         input -= 0.10*usedDime;
         numDime -= usedDime;
-        usedNickel = (int)(input/0.05);
+        if ((int)(input/0.05) <= numNickel)
+            usedNickel = (int)(input/0.05);
+        else
+            usedNickel = numNickel;
         input -= 0.05*usedNickel;
         numNickel -= usedNickel;
     }
@@ -50,13 +65,6 @@ public class Money {
         numQuarters += quarter;
         numDime += dime;
         numNickel += nickel;
-    }
-    public void subtract (int toonie, int loonie, int quarter, int dime, int nickel){
-        numToonies -= toonie;
-        numLoonies -= loonie;
-        numQuarters -= quarter;
-        numDime -= dime;
-        numNickel -= nickel;
     }
     @Override
     public String toString (){
