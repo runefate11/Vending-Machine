d7c468ce17d45a7e5c79622890eee28aa84a5946
GW9999 <GW9999@10.242.70.88>
2015-10-14 11:56:55 -0400

    Still needs stuff?

--- src/vendingmachine/VendingMachine.java
+++ src/vendingmachine/VendingMachine.java
@@ -2,6 +2,7 @@ package vendingmachine;
 
 // variables to create: array of snacks, array of quantities
 // methods: return snacks, make selection, change inventory
+//Devanjith
 public class VendingMachine {
 
     // below is suppose to be an array of objects snacks
